caf6f59f9c14a5fce547ccf580d55f90600e9503
Douglas Dantas <douglasdantas@MacBook-Pro-de-Douglas.local>
2023-07-07 08:53:21 -0300

    feat: structuring folder directory

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const swaggerFile = require('./swagger/swagger_output.json');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css'};
 
-var usersRouter = require('./routes/users');
+const routes = require('./src/routes');
 
 var app = express();
 require('dotenv').config();
@@ -20,10 +19,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {/* #swagger.ignore = true */ res.redirect('/doc'); });
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
-app.use('/users', usersRouter);
+if (process.env.NODE_ENV !== 'test'){
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res) => {/* #swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+}
 
+routes(app);
 
 if (process.env.NODE_ENV !== 'test'){
     const PORT = process.env.PORT || 4000;
